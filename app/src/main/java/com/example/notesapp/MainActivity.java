141a9a125fbd51ed1199f910364d32aa3783c6e4
Ramson <ramson.yasin@gmail.com>
2019-05-15 21:56:21 -0400

    adding a menu option

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.notesapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,6 +15,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     static ArrayList<String> notes = new ArrayList<>(  );
+    static ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = findViewById( R.id.listView );
         notes.add( "Example Notes" );
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, notes);
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, notes);
         listView.setAdapter( arrayAdapter );
 
         listView.setOnItemClickListener( new AdapterView.OnItemClickListener() {
@@ -32,5 +36,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity( intent );
             }
         } );
+    }
+    //MENU
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate( R.menu.add_note_menu,menu );
+        return super.onCreateOptionsMenu( menu );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected( item );
+
+        if(item.getItemId() == R.id.add_note){
+            Intent intent = new Intent( getApplicationContext(),NoteEditorActivity.class );
+            startActivity( intent );
+            return  true;
+        }
+        return  false;
     }
 }
--- app/src/main/java/com/example/notesapp/NoteEditorActivity.java
+++ app/src/main/java/com/example/notesapp/NoteEditorActivity.java
@@ -3,21 +3,44 @@ package com.example.notesapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 
 public class NoteEditorActivity extends AppCompatActivity {
-
+    int noteId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_note_editor );
 
-        EditText editText = findViewById( R.id.editText );
+        EditText editText = findViewById( R.id.editText);
         Intent intent = getIntent();
-        int noteId = intent.getIntExtra( "noteId",-1 );
+        noteId = intent.getIntExtra( "noteId",-1 );
 
         if(noteId != -1){
-            editText.setText( MainActivity.notes.get( noteId ) );
+            editText.setText( MainActivity.notes.get( noteId ));
+        }else{
+            MainActivity.notes.add( "" );
+            noteId = MainActivity.notes.size()-1;
         }
+
+        editText.addTextChangedListener( new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.notes.set(noteId,String.valueOf( s ));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        } );
     }
 }
